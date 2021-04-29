57936f6549e5c460516235136cf5785632477214
Zach Frohardt <zfrohardt@gmail.com>
2021-04-29 10:57:17 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => console.log('happy')} />
   </div>,
   document.getElementById('root')
 );

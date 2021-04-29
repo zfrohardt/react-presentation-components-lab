511850f304fdd362f945bbafaab610ac2d5d977d
Zach Frohardt <zfrohardt@gmail.com>
2021-04-29 10:57:01 -0700

    commit tree 79e95cf601c3d613df03772d8674cdc850a12e25

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,32 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react'
+
+export default class SimpleComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            mood: 'happy',
+        }
+    }
+
+    render() {
+        return (
+            <div onClick={this.clickHandler} >
+                {this.state.mood}
+            </div>
+        );
+    }
+
+    clickHandler = event => {
+        let newMood = '';
+        if (this.state.mood === 'happy') {
+            newMood = 'sad';
+        } else {
+            newMood = 'happy';
+        }
+
+        this.setState({
+            mood: newMood,
+        })
+    }
+}
